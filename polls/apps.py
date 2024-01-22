9328812dc6664dde0c77e83a770c135f6659ef56
Emilio Serna <emilio@vauxoo.com>
2024-01-22 16:21:13 -0700

    [REF] Files now are under pre-commit

--- polls/apps.py
+++ polls/apps.py
@@ -1,6 +1,13 @@
+"""
+This file is used to configure the polls app.
+"""
 from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
+    """
+    This class is used to configure the polls app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "polls"
