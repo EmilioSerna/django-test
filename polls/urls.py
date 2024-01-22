746fd8d232a478dcdd9ec983d5ee03383f912d7f
Emilio Serna <emilio@vauxoo.com>
2024-01-22 16:21:13 -0700

    [REF] Files now are under pre-commit

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,7 @@
+"""
+This module contains the URL configurations for your Django project.
+"""
+
 from django.urls import path
 
 from . import views
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,25 @@
+"""
+This is the views.py file for the polls app.
+"""
 from django.http import HttpResponse
 
 
 def index(request):
+    """
+    This is the index view for the polls app.
+    """
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    """
+    This is the detail view for the polls app.
+    """
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    """
+    This is the results view for the polls app.
+    """
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
