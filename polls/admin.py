8f38b4bc14e365ed831ccddab61295ed9612360d
Emilio Serna <emilio@vauxoo.com>
2024-01-24 20:02:20 -0700

    [IMP] Votes and results

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 """This file is used to register the Question model with the admin site."""
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 admin.site.register(Question)
+admin.site.register(Choice)
