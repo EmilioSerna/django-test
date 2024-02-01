0c517e2f5ef4345ae1fd740f8d5ce375817d5273
Emilio Serna <emilio@vauxoo.com>
2024-01-31 21:47:21 -0700

    [IMP] Changed to generic views

--- polls/views.py
+++ polls/views.py
@@ -4,34 +4,28 @@ This is the views.py file for the polls app.
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-def index(request):
-    """
-    This is the index view for the polls app.
-    """
-    context = {
-        "latest_question_list": Question.objects.order_by("-pub_date")[:5],
-    }
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    """
-    This is the detail view for the polls app.
-    """
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):  # pylint: disable=unused-argument
-    """
-    This is the results view for the polls app.
-    """
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
